24db7cd0d0c327b7e8f484b2e4382904097315fa
PemaPelbar <02240088.cst@rub.edu.bt>
2025-03-13 00:49:35 +0530

    second commit

--- PemaPelbar_02240088_A1_PA.py
+++ PemaPelbar_02240088_A1_PA.py
@@ -1,1 +1,10 @@
-print("hello world")
+import math
+def main():
+    while True:
+        print('Math and string function')
+        print('1.Prime number sum')
+        print('Length converter')
+        print('3.Consonant counter')
+        print('4.')
+    
+            
